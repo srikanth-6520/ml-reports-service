baad0f56e18fd74f7de982256c41d54a5c955164
Shakthieshwari <shakthieshwaria@gmail.com>
2019-09-05 17:36:09 +0530

    Added PDF to Html Converter API

--- controllers/v1/instance_observation.js
+++ controllers/v1/instance_observation.js
@@ -7,10 +7,13 @@ var model = require('../../db')
 var helperFunc = require('../../helper/chartData');
 var commonCassandraFunc = require('../../common/cassandraFunc');
 var ejs = require('ejs')
+var fs = require('fs')
+var uuidv4 = require('uuid/v4');
+var rimraf = require("rimraf");
+const AWS = require('aws-sdk');
 
-
-exports.instanceReport = async function(req,res){
-  if(!req.body.submissionId){
+exports.instanceReport = async function (req, res) {
+  if (!req.body.submissionId) {
     res.status(400);
     var response = {
       result: false,
@@ -32,7 +35,7 @@ exports.instanceReport = async function(req,res){
           options.body = bodyParam;
           var data = await rp(options);
           var responseObj = helperFunc.instanceReportChart(data)
-          if(req.body.download){
+          if (req.body.download) {
             console.log("download");
             responseObj.pdfUrl = "http://www.africau.edu/images/default/sample.pdf"
           }
@@ -55,24 +58,23 @@ exports.instanceReport = async function(req,res){
 
 async function instancePdfFunc(req) {
   return new Promise(function (resolve, reject) {
-  model.MyModel.findOneAsync({ qid: "instance_report_query_staging" }, { allow_filtering: true })
-    .then(async function (result) {
-      var bodyParam = JSON.parse(result.query);
-      console.log(bodyParam)
-      bodyParam.filter.value = req.query.submissionId;
-      //pass the query as body param and get the resul from druid
-      var options = config.options;
-      options.method = "POST";
-      options.body = bodyParam;
-      var data = await rp(options);
-      var responseObj = helperFunc.instanceReportChart(data)
-      // await commonCassandraFunc.insertReqAndResInCassandra(bodyData, responseObj)
-      console.log(responseObj)
-      resolve(responseObj);
-    })
-    .catch(function (err) {
-      reject(err);
-    })
+    model.MyModel.findOneAsync({ qid: "instance_report_query_staging" }, { allow_filtering: true })
+      .then(async function (result) {
+        var bodyParam = JSON.parse(result.query);
+        bodyParam.filter.value = req.query.submissionId;
+        //pass the query as body param and get the resul from druid
+        var options = config.options;
+        options.method = "POST";
+        options.body = bodyParam;
+        var data = await rp(options);
+        var responseObj = helperFunc.instanceReportChart(data)
+        // await commonCassandraFunc.insertReqAndResInCassandra(bodyData, responseObj)
+        // console.log(responseObj)
+        resolve(responseObj);
+      })
+      .catch(function (err) {
+        reject(err);
+      })
   })
 }
 exports.instancePdfReport = async function (req, res) {
@@ -85,7 +87,7 @@ exports.instancePdfReport = async function (req, res) {
     res.send(response);
   } else {
     reqData = req.query
-    console.log(reqData)
+    // console.log(reqData)
     // var dataReportIndexes = await commonCassandraFunc.checkReqInCassandra(reqData)
     // if (dataReportIndexes == undefined) {
     //   var instaRes = instancePdfFunc(req)
@@ -95,8 +97,65 @@ exports.instancePdfReport = async function (req, res) {
     //   // res.send(JSON.parse(dataReportIndexes['apiresponse']))
     // }
     var instaRes = await instancePdfFunc(req)
-    console.log(instaRes['response'].length)
-    res.render('textReport',{instaRes:instaRes['response']})
-    // res.render('index')
+    ejs.renderFile(__dirname + '/../../views/textReport.ejs', { instaRes: instaRes['response'] })
+      .then(function (dataEjsRender) {
+        var dir = __dirname + '/../../tmp/' + uuidv4()
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+        fs.writeFile(dir + '/index.html', dataEjsRender, function (errWriteFile, dataWriteFile) {
+          if (errWriteFile) {
+            throw errWriteFile
+          } else {
+            var optionsHtmlToPdf = config.optionsHtmlToPdf
+            optionsHtmlToPdf.formData = {
+              files: {
+                value: fs.createReadStream(dir + '/index.html'),
+                options: {
+                  filename: 'index.html'
+                }
+              }
+            }
+            rp(optionsHtmlToPdf)
+              .then(function (responseHtmlToPdf) {
+                var pdfBuffer = Buffer.from(responseHtmlToPdf.body)
+                if (responseHtmlToPdf.statusCode == 200) {
+                  fs.writeFile(dir + '/instanceLevelReport.pdf', pdfBuffer, 'binary', function (err) {
+                    if (err) {
+                      return console.log(err);
+                    }
+                    console.log("The PDF was saved!");
+                    const s3 = new AWS.S3(config.s3_credentials);
+                    const uploadFile = () => {
+                      fs.readFile(dir + '/instanceLevelReport.pdf', (err, data) => {
+                        if (err) throw err;
+                        const params = {
+                          Bucket: config.s3_bucketName, // pass your bucket name
+                          Key: 'instanceLevelPdfReports/instanceLevelReport.pdf', // file will be saved as testBucket/contacts.csv
+                          Body: JSON.stringify(data, null, 2)
+                        };
+                        s3.upload(params, function (s3Err, data) {
+                          if (s3Err) throw s3Err
+                          console.log(`File uploaded successfully at ${data.Location}`)
+                        });
+                      });
+                    };
+
+                    uploadFile();
+                  });
+                }
+              })
+              .catch(function (err) {
+                console.log("error in converting HtmlToPdf")
+                throw err;
+              });
+          }
+        })
+        //delete the directory once stored into s3
+        // rimraf.sync(dir);
+      })
+      .catch(function (errEjsRender) {
+        console.log(errEjsRender)
+      })
   }
 }
--- routes/api_router.js
+++ routes/api_router.js
@@ -5,12 +5,12 @@ var instanceController = require('../controllers/v1/instance_observation');
 var entityController = require('../controllers/v1/entity_observations')
 var observationController = require('../controllers/v1/observation_controller')
 
-<<<<<<< HEAD
 //sl_assessment query
-router.post("/slassessment",myController.slAssessment);
-router.post("/observations/instance",myController.instanceReport);
-router.get('/observations/instanceLevelPdfReports',myController.instancePdfReport)
-=======
+// router.post("/slassessment",instanceController.slAssessment);
+// router.post("/observations/instance",instanceController.instanceReport);
+
+
+router.get('/observations/instanceLevelPdfReports',instanceController.instancePdfReport)
 //API router for observations instanceReport
 router.post("/observations/instance",instanceController.instanceReport);
 
@@ -20,8 +20,7 @@ router.post("/observations/entity",entityController.entityReport);
 //API router for observationReport
 router.post("/observations/report",observationController.observationReport);
 
-//API router for HM view 
+//API router for HM view
 router.post("/assessments/school",observationController.observationReport);
->>>>>>> bfcee26ada06260075b2e225ae52557c76199950
 
 module.exports = router;
