9ca1c4efd33e29a59ede200fd9ac908b0dc2f385
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-10-09 19:15:10 +0530

    Api is updated

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,12 +1,10 @@
 const aws = require('aws-sdk');
-const accessKeyId = process.env.ACCESS_KEY_ID;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-const bucketName = process.env.OUTPUT_BUCKET;
+const config = require('./config.json');
 
-var s3 = new aws.S3({ 'accessKeyId': accessKeyId, 'secretAccessKey': secretAccessKey });
+var s3 = new aws.S3({ 'accessKeyId': config.s3_credentials.accessKeyId, 'secretAccessKey': config.s3_credentials.secretAccessKey });
 
 var getParams = {
-    Bucket: bucketName, //replace example bucket with your s3 bucket name
+    Bucket: config.s3_bucketName, //replace example bucket with your s3 bucket name
     Key: '' // replace file location with your s3 file location
 };
 
