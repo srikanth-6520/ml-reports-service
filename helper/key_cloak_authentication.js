cebb66ed2b15f5a49e34851931db9e8e68937548
Priyanka Pradeep <priyanka@tunerlabs.com>
2020-10-12 14:14:38 +0530

    Offline validation

--- helper/key_cloak_authentication.js
+++ helper/key_cloak_authentication.js
@@ -3,6 +3,10 @@
  * author : 
  **/
 var keyCloakAuthUtils = require("keycloak-auth-utils");
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const accessTokenValidationMode = (process.env.VALIDATE_ACCESS_TOKEN_OFFLINE && process.env.VALIDATE_ACCESS_TOKEN_OFFLINE === "OFF")? "OFF" : "ON";
+const keyCloakPublicKeyPath = (process.env.KEYCLOAK_PUBLIC_KEY_PATH && process.env.KEYCLOAK_PUBLIC_KEY_PATH != "") ? ROOT_PATH+"/"+process.env.KEYCLOAK_PUBLIC_KEY_PATH : ROOT_PATH+"/"+"keycloak-public-keys/" ;
 
 function ApiInterceptor(keycloak_config, cache_config) {
   this.config = keycloak_config;
@@ -18,9 +22,46 @@ function ApiInterceptor(keycloak_config, cache_config) {
  * @return {[Function]} callback [its retrun err or object with fields(token, userId)]
  */
 ApiInterceptor.prototype.validateToken = function(token, callback) {
-      var self = this;
 
+  if (accessTokenValidationMode === "ON") {
+    var self = this;
+    var decoded = jwt.decode(token, { complete: true });
+    const kid = decoded.header.kid
+    let cert = "";
+    let path = keyCloakPublicKeyPath + kid + '.pem';
+    cert = fs.readFileSync(path);
 
+    if (fs.existsSync(path)) {
+      jwt.verify(token, cert, { algorithm: 'RS256' }, function (err, decode) {
+  
+        if (err) {
+          return callback(err.message, null);
+        }
+
+        if (decode !== undefined) {
+          const expiry = decode.exp;
+          const now = new Date();
+          if (now.getTime() > expiry * 1000) {
+            return callback('Expired', null);
+          }
+
+          self.grantManager.userInfo(token, function(err, userData) {
+            if (err) {
+              return callback(err, null);
+            } else {
+              return callback(null, { token: token, userId: userData.sub.split(":").pop() });
+            }
+          });
+
+        } else {
+          return callback(err, null);
+        }
+
+      });
+    } else {
+      return callback(err, null);
+    }
+  }else{
       self.grantManager.userInfo(token, function(err, userData) {
         if (err) {
           return callback(err, null);
@@ -28,6 +69,7 @@ ApiInterceptor.prototype.validateToken = function(token, callback) {
           return callback(null, { token: token, userId: userData.sub.split(":").pop() });
         }
       });
+    }    
 };
 
 module.exports = ApiInterceptor;
