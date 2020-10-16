680e552ab70adf5a411c54ff275d2cae9cb66656
shrawanTibil <shrawan.b@tibilsolutions.com>
2020-10-16 10:51:39 +0530

    learning-api

---
+++ controllers/v1/learning_group_participation_percentage.js
@@ -0,0 +1,31 @@
+const auth = require('../../middleware/authentication_service');
+var const_data = require('../../config/aws-config');
+const csv = require('csvtojson')
+exports.learning_group_participation_percentage = async function (req, res) {
+    try{
+    const_data['getParams']['Key'] = 'mantra/group_participation_percentage/group_participation_percentage.csv'
+    async function csvToJSON() {
+        var params = {
+            Bucket: const_data['getParams']['Bucket'],
+            Key: const_data['getParams']['Key'],
+          };
+          const_data['s3'].headObject(params, async function (err, metadata) {
+            if (err && err.code === 'NotFound') {
+              res.status(403).json({ errMsg: 'No such data found' });
+            } else {
+              const stream = const_data['s3'].getObject(const_data['getParams']).createReadStream();
+              const json = await csv().fromStream(stream);
+              res.status(200).send(json);
+            }
+          });
+    }
+    csvToJSON()
+}
+catch (e) {
+    console.log(e);
+    res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+}
+
+     
+    };
+
