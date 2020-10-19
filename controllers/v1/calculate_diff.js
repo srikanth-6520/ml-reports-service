7959576518b96174595f5abe520e84caaf0a7111
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-10-19 18:08:42 +0530

    API for Quiz percentage varience

---
+++ controllers/v1/calculate_diff.js
@@ -0,0 +1,18 @@
+const auth = require('../../middleware/authentication_service');
+var const_data = require('../../config/aws-config');
+const csv = require('csvtojson');
+exports.diff = async function (req, res) {
+    try {
+        var difference = [];
+        var data1 = req.body.data.data1;
+        var data2 = req.body.data.data2;
+        for (let i = 0; i < data1.length; i++) {
+            var mydiff = parseFloat(((data2[i] - data1[i]) * 100 / data1[i]).toFixed(2));
+            difference.push(mydiff);
+        }
+        res.status(200).json(difference);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ errMessage: 'Internal error. Please try again!!' });
+    }
+};
