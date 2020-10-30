1c174b8d782139850f4071670580d794c2d83b65
dheeraj8008tiwari <dheeraj.tiwari@tibilsolutions.com>
2020-10-30 21:16:24 +0530

    api's updated

---
+++ controllers/v1/mantra4changeApi.js
@@ -0,0 +1,164 @@
+const readFile = require('../v1/portal_reports/read_file')
+const files = require('./portal_reports/filePaths');
+var groupBy = require('group-array')
+/**
+   * @api {get} /dhiti/api/:version/:controller/:method
+   * @apiVersion 1.0.0
+   * @apiGroup Shikshalokam 
+   * @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       "result": true,
+*       "data": [{
+        
+        }]
+*     }
+   * @apiUse errorBody
+   */
+exports.genericApi = async function (req, res) {
+    try {
+        var fileName = files[`${req.body.key}`];
+        var result = await readFile.readS3File(fileName);
+        res.send({ "result": true, "data": result })
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+
+};
+/**
+   * @api {get} /dhiti/api/:version/:controller/:method
+   * @apiVersion 1.0.0
+   * @apiGroup Shikshalokam 
+   * @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       "result": true,
+*       "data": [{
+               "variance":[]
+        
+        }]
+*     }
+   * @apiUse errorBody
+   */
+exports.percentageVariance = async function (req, res) {
+    try {
+        console.log(req.body.data);
+        var difference = [];
+        var data1 = req.body.data.data1;
+        var data2 = req.body.data.data2;
+        for (let i = 0; i < data1.length; i++) {
+            var mydiff = parseFloat(((data2[i] - data1[i]) * 100 / data1[i]).toFixed(2));
+            difference.push(mydiff);
+        }
+        res.status(200).json({ 'result': true, 'data': difference });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ errMessage: 'Internal error. Please try again!!' });
+    }
+};
+
+/**
+   * @api {get} /dhiti/api/:version/:controller/:method
+   * @apiVersion 1.0.0
+   * @apiGroup Shikshalokam 
+   * @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       "result": true,
+*       "data": [{
+               "group:"",
+               "count":[]
+        
+        }]
+*     }
+   * @apiUse errorBody
+   */
+
+exports.multiResource = async function (req, res) {
+    try {
+        var fileName = files[`${req.body.key}`];
+        let resources = req.body.data;
+        const jsonData = await readFile.readS3File(fileName);
+        var jdata = []
+        await jsonData.forEach(data => {
+            resources.forEach(item => {
+                if (data.content_name == item) {
+                    jdata.push(data);
+                }
+            });
+        });
+        let groupData = groupBy(jdata, 'role_externalId');
+        let myKeys = Object.keys(groupData);
+        let usersCount = []
+        myKeys.forEach(key => {
+            var grouppedData = groupBy(groupData[`${key}`], 'User_FirstName');
+            var userKeys = Object.keys(grouppedData);
+            var cnt = 0;
+            userKeys.forEach(user => {
+                if (grouppedData[`${user}`].length == resources.length) {
+                    cnt++;
+                }
+            });
+            usersCount.push({ group: key, count: cnt });
+        })
+        console.log(usersCount);
+        res.send({ "result": true, "data": usersCount })
+    }
+
+    catch (e) {
+        console.log(e);
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+
+};
+/**
+   * @api {get} /dhiti/api/:version/:controller/:method
+   * @apiVersion 1.0.0
+   * @apiGroup Shikshalokam 
+   * @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       "result": true,
+*       "data": [{
+               "name":"",
+               "data":[]
+        
+        }]
+*     }
+   * @apiUse errorBody
+   */
+
+exports.multiSelection = async function (req, res) {
+    try {
+        var fileName = files[`${req.body.key}`];
+        let list1 = req.body.data;
+        const jsonData = await readFile.readS3File(fileName);
+        var jdata = []
+        var keys = Object.keys(jsonData[0]);
+        await list1.forEach(item => {
+            var mydata = [];
+            jsonData.forEach(data => {
+                keys.forEach(key => {
+                    if (item == key) {
+                        mydata.push(Number(data[`${key}`]));
+
+                    }
+                })
+            });
+            let a = {
+                name: item,
+                data: mydata
+            }
+            jdata.push(a)
+        })
+
+        res.send({ "result": true, "data": jdata })
+    }
+
+    catch (e) {
+        console.log(e);
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+};
---
+++ controllers/v1/portal_reports/filePaths.js
@@ -0,0 +1,24 @@
+let userNeverLoggedIn = 'mantra/Adoption_Dashboard/users_never_logged_in/users_never_logged_in.json';
+let uniqueActiveUsers = 'mantra/Home_Dashboard/landing_metric/app_usage.json';
+let topScoreQuiz = 'mantra/Home_Dashboard/top_scorers_quiz/top_scorers_quiz.json';
+let topFiveContentRatings = 'mantra/Program_Effectiveness/top_content_ratings/top_five_content_ratings.json';
+let mapDataResources = 'mantra/Home_Dashboard/maps/resource_usage.json';
+let loginTrend = 'mantra/Adoption_Dashboard/compliance/loggingtrend/monthly/september_latest/logging_trend_september.json';
+let loginPercentage = 'mantra/Home_Dashboard/maps/users_usage2.json';
+let learningTopscoreQuiz = 'mantra/Learning/top_scorers_quiz_percentage/top_scorers_quiz_percentage.json';
+let participationPercentage = 'mantra/Learning/group_participation_percentage/group_participation_percentage.json';
+let lastUpdatedDate = 'mantra/Home_Dashboard/date/last_updated_date.json';
+let dailyAverageGrowth = 'mantra/Home_Dashboard/compliance/loggingtrend/monthly/september_latest/daily_activity_trend.json';
+let countContentRating = 'mantra/Program_Effectiveness/top_content_ratings/count_content_ratings_with_avg.json';
+let averageTimeSpent = 'mantra/Home_Dashboard/timespent/avg_timespent.json';
+let averageRatingContent = 'mantra/Home_Dashboard/top_content_ratings/top_content_ratings.json';
+let appCount = 'mantra/Adoption_Dashboard/compliance/loggingtrend/monthly/september_latest/daily_distinct_users_trend.json';
+let adoptionContent = 'mantra/Adoption_Dashboard/user_content_watch/user_content_watch.json';
+let multiResource = 'mantra/Adoption_Dashboard/user_content_watch/user_content_watch.json';
+let multiSelection = 'mantra/Adoption_Dashboard/compliance/loggingtrend/monthly/september_latest/logging_trend_september.json';
+
+module.exports = {
+    userNeverLoggedIn, uniqueActiveUsers, topScoreQuiz, topFiveContentRatings, mapDataResources,
+    loginTrend, loginPercentage, learningTopscoreQuiz, participationPercentage, lastUpdatedDate, dailyAverageGrowth,
+    countContentRating, averageRatingContent, averageTimeSpent, appCount, adoptionContent, multiResource, multiSelection
+}
