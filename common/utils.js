e3b03834f732e14f75e144e6fc421c6d59ebe68b
Akash Shah <aks301190@gmail.com>
2021-06-03 16:08:05 +0530

    Update utils.js

--- common/utils.js
+++ common/utils.js
@@ -63,50 +63,9 @@ function getGotenbergConnection() {
   return options;
 }
 
-/**
-  * Return aws connection credentials
-  * @function
-  * @name getAWSConnection
-  * @returns {Array}  returns aws connection.  
-*/
-
-/**
-  * Returns file path of given fileName
-  * @function
-  * @name getPortalReportsFilePaths
-  * @returns {Array}  returns file path of given fileName.  
-*/
-
-function getPortalReportsFilePaths(fileName) {
-
-  let file_path = {
-    userNeverLoggedIn: process.env.USER_NEVER_LOGGED_IN,
-    uniqueActiveUsers: process.env.UNIQUE_ACTIVE_USERS,
-    topScoreQuiz: process.env.TOP_SCORE_QUIZ,
-    topFiveContentRatings: process.env.CONTENT_RATINGS,
-    mapDataResources: process.env.MAP_DATA_RESOURCES,
-    loginTrend: process.env.LOGIN_TREND,
-    loginPercentage: process.env.LOGIN_PERCENTAGE,
-    learningTopscoreQuiz: process.env.LEARNING_TOP_SCORE_QUIZ,
-    participationPercentage: process.env.PARTICIPATION_PERCENTAGE,
-    lastUpdatedDate: process.env.LAST_UPDATED_DATE,
-    dailyAverageGrowth: process.env.DAILY_AVERAGE_GROWTH,
-    countContentRating: process.env.COUNT_CONTENT_RATING,
-    averageTimeSpent: process.env.AVERAGE_TIME_SPENT,
-    averageRatingContent: process.env.AVERAGE_RATING_CONTENT,
-    appCount: process.env.APP_COUNT,
-    adoptionContent: process.env.ADOPTION_CONTENT,
-    multiResource: process.env.MULTI_RESOURCE,
-    multiSelection: process.env.MULTI_SELECTION
-  }
-
-  return file_path[fileName];
-}
-
 
 module.exports = {
   getDruidQuery: getDruidQuery,
   getDruidConnection: getDruidConnection,
-  getGotenbergConnection: getGotenbergConnection,
-  getPortalReportsFilePaths: getPortalReportsFilePaths
+  getGotenbergConnection: getGotenbergConnection
 }
